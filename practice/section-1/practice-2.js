6e98d76d74370dfff097c836df844a89f4531571
caohedan <a851107622a@qq.com>
2018-07-03 20:05:21 +0800

    Update practice-2.js

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,15 +1,22 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-	  //循环判断数组a里的元素在b里面有没有，有的话就放入新建立的数组中
+
             var result = new Array();
+            //将b集合的每个元素转为字符串
+            for(var i=0;i<collectionB.length;i++){
+                collectionB[i]=collectionB[0].toString();
+            }
             var c=collectionB[0].toString();
             for(var i=0;i<collectionA.length;i++)
             {
-              if(c.indexOf(collectionA[i].toString())>-1)
-              {
-              result.push(collectionA[i]);
-              }
+               for(var j=0;j<collectionB.length;j++){
+                if(collectionB[j].indexOf(collectionA[i].toString())>-1)
+                             {
+                             result.push(collectionA[i]);
+                             }
+               }
+
             }
             return result;
 
